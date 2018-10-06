73b4f2cdf3b6922653e9310dddb462d71f9860bb
Quinn Lu <luqian.ncsu@gmail.com>
2018-10-05 23:46:23 -0700

    LintCode 76: Longest Increasing Subsequence

---
+++ LintCode/76_Longest-Increasing-Subsequence/76_Longest-Increasing-Subsequence-V2.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <vector>
+#include<limits.h>
+
+using namespace std;
+
+// find the first number > num
+int binarySearch(vector<int> & minLast, int num) {
+    int start = 0, end = minLast.size() - 1;
+    while (start + 1 < end) {
+        int mid = (end - start) / 2 + start;
+        if (minLast[mid] < num) {
+            start = mid;
+        } else {
+            end = mid;
+        }
+    }
+
+    return end;
+}
+
+int longestIncreasingSubsequence(vector<int> & nums) {
+
+    vector<int> minLast(nums.size() + 1, 0);
+    minLast[0] = INT_MIN;
+
+    for (int i = 1; i <= nums.size(); i++) {
+        minLast[i] = INT_MAX;
+    }
+
+    for (int i = 0; i < nums.size(); i++) {
+        // find the first number in minLast >= nums[i]
+        int index = binarySearch(minLast, nums[i]);
+        cout<<"i = "<<i<<" index = "<<index<<endl;
+        minLast[index] = nums[i];
+    }
+
+    for (int i = nums.size(); i >= 1; i--) {
+        if (minLast[i] != INT_MAX) {
+                //debug
+            cout<<"minLast: ";
+            for (int m = 0; m < minLast.size(); ++m)
+                cout<<minLast[m]<<" ";
+            cout<<endl;
+
+            return i;
+        }
+    }
+
+    return 0;
+}
+
+int main()
+{
+  //  vector<int> nums = {2, 3, 1, 7, 6, 4, 8};
+    vector<int> nums = {2, 3, 1};
+    int result;
+    result = longestIncreasingSubsequence(nums);
+    cout<<result<<endl;
+}
