32a872a1c5565fa100a475c82310dee3b4264b4d
Quinn Lu <Qian.Lu@wdc.com>
2018-10-14 00:06:37 -0700

    LintCode 8: Rotate String

--- LintCode/39_Recover-Rotated-Sorted-Array/README.md
+++ LintCode/39_Recover-Rotated-Sorted-Array/README.md
@@ -2,4 +2,4 @@ Three-step rotation.
 Time complexity O(n). 
 Space complexity O(1).
 
-One minor optimization is the finding of pivot position can be done via binary search. But the time complexity is still O(n).
+One minor optimization is the finding of pivot position can be done via binary search (see LintCode 62). But the time complexity is still O(n).
---
+++ LintCode/8_Rotate-String/8_Rotate-String.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    /**
+     * @param str: An array of char
+     * @param offset: An integer
+     * @return: nothing
+     */
+    void rotateString(string &str, int offset) {
+        //string str1 = str.substr(0, str.size() - offset);
+        //string str2 = str.substr(str.size() - offset);
+        if (str.size() == 0) return;
+        offset = offset % str.size();
+        reverse(str, str.size() - offset, str.size() - 1);
+        reverse(str, 0, str.size() - offset - 1);
+        reverse(str, 0, str.size() - 1);
+    }
+    
+private:
+    void reverse(string &str, int start, int end) {
+        while(start < end) {
+            char temp = str[start];
+            str[start] = str[end];
+            str[end] = temp;
+            start++;
+            end--;
+        }
+    }
+};
