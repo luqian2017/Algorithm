68d058b1faa70f8e8288129823de00dccd664676
luqian2017 <luqian.ncsu@gmail.com>
2018-10-15 23:47:49 -0700

    LintCode 798: Backpack VII

---
+++ LintCode/798_Backpack-VII/798_Backpack-VII.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    /**
+     * @param n: the money of you
+     * @param prices: the price of rice[i]
+     * @param weight: the weight of rice[i]
+     * @param amounts: the amount of rice[i] 
+     * @return: the maximum weight
+     */
+    int backPackVII(int n, vector<int> &prices, vector<int> &weight, vector<int> &amounts) {
+        int itemCount = prices.size();   //count of items
+        vector<vector<int>> dp(itemCount + 1, vector<int>(n + 1, 0)); //dp[i][j] means the maximum weight of first i tems with total price <= j
+        
+        for (int i = 1; i <= itemCount; ++i) { // i->item[i]
+            for (int k = 1; k <=n; ++k) {
+                dp[i][k] = dp[i - 1][k];
+            }
+
+            for (int j = 0; j <= amounts[i - 1]; ++j) { //j-> amount[0]..amount[i-1]
+                for (int k = 1; k <= n; ++k) { //k->  1 .. n
+                    if (k >= j * prices[i - 1]) {
+                        dp[i][k] = max(dp[i][k], dp[i - 1][k - j * prices[i - 1]] + j * weight[i - 1]);
+                    }
+                }
+            }
+        }
+
+        return dp[itemCount][n];
+    }
+};
---
+++ LintCode/798_Backpack-VII/README.md
@@ -0,0 +1,27 @@
+Solution 1. Classical DP solution, based on 01 backpack problem.
+Note:
+1)
+   dp[0..itemCount][0..n]
+   weight[0..itemCount-1]
+   prices[0..itemCount-1]
+   amounts[0..itemCount-1]
+   
+   dp[i - 1][k - j * prices[i - 1]] + j * weight[i - 1];
+   here the first "i - 1" means the first i - 1 items, the second and the third "i-1" actually means the ith item (as the array index starts from 0).
+   
+2) Fot the three-layer loop, it is wrong to right it as follows:
+      
+           for (int i = 1; i <= itemCount; ++i) { // i->item[i]
+            for (int j = 0; j <= amounts[i - 1]; ++j) { //j-> amount[0]..amount[i-1]
+                for (int k = 1; k <= n; k++) { //k->  1 .. n
+                    dp[i][k] = dp[i - 1][k];
+                    
+                    if (k >= j * prices[i - 1]) {
+                        dp[i][k] = max(dp[i][k], dp[i - 1][k - j * prices[i - 1]] + j * weight[i - 1]);
+                    }
+                }
+            }
+        }
+		
+	First, it wastes time as dp[i][k] = dp[i - 1][k]; as executed by the j loop which is unecessary.	
+	Second, dp[i][k] is updated incorrectly.
