42b43a7c330247b850b1e241ec4dbb274869a3f5
luqian2017 <luqian.ncsu@gmail.com>
2018-10-17 23:18:43 -0700

    LintCode 92: Backpack

---
+++ LintCode/92_Backpack/92_Backpack.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    /**
+     * @param m: An integer m denotes the size of a backpack
+     * @param A: Given n items with size A[i]
+     * @return: The maximum size
+     */
+    int backPack(int m, vector<int> &A) {
+        int n = A.size();
+        vector<int> dp(m + 1, 0);
+        
+        for (int i = 0; i < n; ++i) {
+            //for (int k = m; k >= A[i]; --k) {
+            for (int k = A[i]; k <= m; ++k) {
+                dp[k] = max(dp[k], dp[k - A[i]] + A[i]);
+            }
+        }
+
+        for (int k = 0; k <= m; ++k)
+            cout<<dp[k]<<" ";
+            
+        cout<<endl;
+        
+        return dp[m];
+    }
+};
---
+++ LintCode/92_Backpack/README.md
@@ -0,0 +1,4 @@
+Typical 01 backpack problem.
+The solution is to use the one-dimension optimization.
+
+Note that the k loop should be reversed: i.e., from big to small.
