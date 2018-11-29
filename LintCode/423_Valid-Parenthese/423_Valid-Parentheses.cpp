9111b16c784ca6604679c587eeeb6e0e6b49d417
luqian2017 <luqian.ncsu@gmail.com>
2018-11-28 20:51:52 -0800

    LintCode 423

---
+++ LintCode/423_Valid-Parenthese/423_Valid-Parentheses.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    /**
+     * @param s: A string
+     * @return: whether the string is a valid parentheses
+     */
+    bool isValidParentheses(string &s) {
+        int len = s.size();
+        stack<char> st; 
+        
+        for (int i = 0; i < len; ++i) {
+            if ((s[i] == '(') ||
+                (s[i] == '{') ||
+                (s[i] == '[')) {
+                st.push(s[i]);
+                continue;
+            }
+            
+            if (st.empty()) return false;
+            
+            if (s[i] == ')') {
+                if (!st.empty()) {
+                    if (st.top() != '(') return false;
+                    else st.pop();
+                }
+            } else if (s[i] == '}') {
+                if (!st.empty()) {
+                    if (st.top() != '{') return false;
+                    else st.pop();
+                }
+            } else if (s[i] == ']') {
+                if (!st.empty()) {
+                    if (st.top() != '[') return false;
+                    else st.pop();
+                }
+            }
+        }
+    
+        if (!st.empty()) return false;
+        return true;
+    }
+};
