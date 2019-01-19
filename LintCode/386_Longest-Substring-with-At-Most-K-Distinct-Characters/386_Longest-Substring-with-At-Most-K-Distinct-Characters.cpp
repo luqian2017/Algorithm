5ebfd32562791ba96f3ac13248423545c4cc69c3
luqian2017 <luqian.ncsu@gmail.com>
2019-01-19 11:18:50 -0800

    LintCode 386

--- LintCode/386_Longest-Substring-with-At-Most-K-Distinct-Characters/386_Longest-Substring-with-At-Most-K-Distinct-Characters.cpp
+++ LintCode/386_Longest-Substring-with-At-Most-K-Distinct-Characters/386_Longest-Substring-with-At-Most-K-Distinct-Characters.cpp
@@ -1,31 +1,26 @@
 class Solution {
 public:
-    /**
-     * @param s: A string
-     * @param k: An integer
-     * @return: An integer
-     */
-    int lengthOfLongestSubstringKDistinct(string &s, int k) {
-        int len = s.size();
-        if ((len == 0) || (len < k)) return 0;
-        
+    int lengthOfLongestSubstringKDistinct(string s, int k) {
+        int maxLen = 0;
         int l = 0, r = 0;
-        int count = 0; //num of distinct characters
-        int maxLen = 1;
-        vector<int> dict(256, 0); //records the freqs of each ASCII character
+        int len = s.size();
+        if (len == 0 || k == 0) return 0;
         
-        for (r = 0; r < len; ++r) {
-            dict[s[r]]++;
+        vector<int> dict(256, 0);
+        int count = 0;
+        
+        for (int r = 0; r < len; ++r) {
+            ++dict[s[r]];
             if (dict[s[r]] == 1) count++;
             
-            while(count >= k && l < r) {
-                maxLen = max(maxLen, r - l + 1);
+            while (count > k) {
                 dict[s[l]]--;
                 if (dict[s[l]] == 0) count--;
                 ++l;
             }
+            
+            maxLen = max(maxLen, r - l + 1);
         }
-        
         return maxLen;
     }
 };
