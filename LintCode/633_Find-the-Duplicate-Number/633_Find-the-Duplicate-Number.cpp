f8ffaf649b5758966c8416416934bcef07a67042
luqian2017 <luqian.ncsu@gmail.com>
2019-02-12 23:23:52 -0800

    LintCode 633

---
+++ LintCode/633_Find-the-Duplicate-Number/633_Find-the-Duplicate-Number.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    /**
+     * @param nums: an array containing n + 1 integers which is between 1 and n
+     * @return: the duplicate one
+     */
+    int findDuplicate(vector<int> &nums) {
+        int len = nums.size();
+        int start = 1, mid = 0, end = len - 1;
+        while (start + 1 < end) {
+            mid = start + (end - start) / 2;
+            if (calCount(nums, mid) > mid) { // it shows the mid is to the left of the duplicate
+                end = mid;
+            } else {
+                start = mid;
+            }
+        }
+        if (calCount(nums, start) > start) return start;
+        return end;      
+    }
+
+private:
+    int calCount(vector<int> &nums, int n) {
+        int count = 0;
+        int len = nums.size();
+        for (int i = 0; i < len; ++i) {
+            if (nums[i] <= n) count++;   //it should be <= here! not <
+        }
+        return count;
+    }
+};
