c27fa5e113774d7a6025d26813a6a1f2be3dfad8
luqian2017 <luqian.ncsu@gmail.com>
2019-02-26 00:03:18 -0800

    LintCode 392

---
+++ LintCode/392_House-Robber/392_House-Robber.cpp
@@ -0,0 +1,23 @@
+
+class Solution {
+public:
+    /**
+     * @param A: An array of non-negative integers
+     * @return: The maximum amount of money you can rob tonight
+     */
+    long long houseRobber(vector<int> &A) {
+        int len = A.size();
+        if (len == 0) return 0;
+        if (len == 1) return A[0];
+        
+        vector<long long> dp(len, 0);
+        dp[0] = A[0];
+        dp[1] = A[0] > A[1] ? A[0] : A[1];
+        
+        for (int i = 2; i < len; ++i) {
+            dp[i] = dp[i - 2] + A[i] > dp[i - 1] ? dp[i - 2] + A[i] : dp[i - 1];
+        }
+        
+        return dp[len - 1];
+    }
+};
