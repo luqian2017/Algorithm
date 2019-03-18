3f020dff77b09c8992a1c18c9d416e401a8c5473
luqian2017 <luqian.ncsu@gmail.com>
2019-03-18 00:41:17 -0700

    LintCode 403

---
+++ LintCode/403_Continuous-Subarray-Sum-II/403_Continuous-Subarray-Sum-II.cpp
@@ -0,0 +1,74 @@
+class Solution {
+public:
+    /*
+     * @param A: An integer array
+     * @return: A list of integers includes the index of the first number and the index of the last number
+     */
+    vector<int> continuousSubarraySumII(vector<int> &A) {
+        int n = A.size();
+        if (n == 0) return vector<int>();
+        
+        int sum = A[0], maxSum = A[0], minSum = A[0], totalSum = A[0];
+        int maxStartIndex = 0, maxEndIndex = 0;
+        int gMaxStartIndex = 0, gMaxEndIndex = 0;
+        int minStartIndex = 0, minEndIndex = 0;
+        int gMinStartIndex = 0, gMinEndIndex = 0;
+        
+        for (int i = 1; i < n; ++i) {
+            totalSum += A[i];
+            if (sum < 0) {
+                sum = A[i];
+                maxStartIndex = i;
+                maxEndIndex = i;
+            } else {
+                sum += A[i];
+                maxEndIndex = i;
+            } 
+                
+            if (sum > maxSum) {
+                maxSum = sum;
+                gMaxStartIndex = maxStartIndex;
+                gMaxEndIndex = maxEndIndex;
+            }
+        }
+        
+        sum = A[0];
+        for (int i = 1; i < n; ++i) {
+            if (sum > 0) {
+                sum = A[i];
+                minStartIndex = i;
+                minEndIndex = i;
+            } else {
+                sum += A[i];
+                minEndIndex = i;
+            }
+            
+            if (sum < minSum) {
+                minSum = sum;
+                gMinStartIndex = minStartIndex;
+                gMinEndIndex = minEndIndex;
+            }
+        }
+        
+        if (maxSum > totalSum - minSum) {
+            return vector<int>{gMaxStartIndex, gMaxEndIndex};
+        } else {
+            if (gMinStartIndex == 0 && gMinEndIndex == n - 1) {
+                int maxElem = INT_MIN;
+                int maxElemIndex = 0;
+                for (int i = 0; i < n; ++i) {
+                    if (maxElem < A[i]) {
+                        maxElem = A[i];
+                        maxElemIndex = i;
+                    }            
+                }
+                return vector<int>{maxElemIndex, maxElemIndex};
+                
+            } else {
+                return vector<int>{(gMinEndIndex + 1) % n, (gMinStartIndex - 1 + n) % n};
+            }
+        }
+        
+        
+    }
+};
---
+++ LintCode/403_Continuous-Subarray-Sum-II/README.md
@@ -0,0 +1,18 @@
+403. Continuous Subarray Sum II
+
+Given an circular integer array (the next element of the last element is the first element), find a continuous subarray in it, where the sum of numbers is the biggest. Your code should return the index of the first number and the index of the last number.
+
+If duplicate answers exist, return any of them.
+
+Example
+Example 1:
+
+Input: [3, 1, -100, -3, 4]
+Output: [4, 1]
+Example 2:
+
+Input: [1,-1]
+Output: [0, 0]
+Challenge
+O(n) time
+
