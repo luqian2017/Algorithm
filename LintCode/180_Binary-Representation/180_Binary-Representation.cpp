136529f87deb8be554364b2e540c8cd9c0cbfa20
luqian2017 <luqian.ncsu@gmail.com>
2019-05-27 01:20:03 -0700

    LintCode 180

---
+++ LintCode/180_Binary-Representation/180_Binary-Representation.cpp
@@ -0,0 +1,61 @@
+class Solution {
+public:
+    /**
+     * @param n: Given a decimal number that is passed in as a string
+     * @return: A string
+     */
+    string binaryRepresentation(string &n) {
+        
+        int strSize = n.size();
+        
+        if (strSize == 0) return "";
+        
+        int ptPos = n.find_first_of('.');
+        
+        int intV = 0;
+        double fracV = 0.0;
+        
+        if (ptPos != string::npos) {
+            intV = atoi(n.substr(0, ptPos).c_str());
+            fracV = atof(n.substr(ptPos).c_str());
+        } else {
+            intV = atoi(n.c_str());
+        }
+
+        string intBinStr = "";
+        while(intV) {
+            if (intV & 0x1) { 
+                intBinStr = '1' + intBinStr;
+            } else {
+                intBinStr = '0' + intBinStr;
+            }
+            intV >>= 1;
+        }
+        
+        //don't forget.
+        if (intBinStr == "") intBinStr = "0";
+        
+        if (fracV == 0) return intBinStr;
+        
+        string fracBinStr = "";
+        while (fracV) {
+            if (fracBinStr.size() > 32) return "ERROR";
+            
+            //important!!!
+            if (fracV == 1) {
+                fracBinStr += '1';
+                break;
+            }
+            
+            fracV *= 2;
+            if (fracV >= 1) {
+                fracBinStr += '1';
+                fracV -= 1;
+            } else {
+                fracBinStr += '0';
+            }
+        }
+        
+        return intBinStr + '.' + fracBinStr;
+    }
+};
---
+++ LintCode/180_Binary-Representation/README.md
@@ -0,0 +1,25 @@
+180. Binary Representation
+中文English
+Given a (decimal - e.g. 3.72) number that is passed in as a string, return the binary representation that is passed in as a string. If the fractional part of the number can not be represented accurately in binary with at most 32 characters, return ERROR.
+
+Example
+Example 1
+
+Input: "3.72"
+Output: "ERROR"
+Explanation: (3.72)_{10} = (11.10111000010100011111\cdots)_2(3.72)
+​10
+​​ =(11.10111000010100011111⋯)
+​2
+​​  We can't represent it in 32 characters.
+
+Example 2
+
+Input: "3.5"
+OUtput: "11.1"
+Explanation: (3.5)_{10}=(11.1)_2(3.5)
+​10
+​​ =(11.1)
+​2
+​​ 
+
