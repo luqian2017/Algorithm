0022401fbe1fe5c94354cd2fa428302fe770b312
luqian2017 <luqian.ncsu@gmail.com>
2019-08-21 23:51:58 -0700

    LintCode 522

---
+++ LintCode/522_Reverse-Pairs/522_Reverse-Pairs.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    /**
+     * @param A: an array
+     * @return: total of reverse pairs
+     */
+    long long reversePairs(vector<int> &A) {
+        vector<int> sortedA = A;
+        sort(sortedA.begin(), sortedA.end());
+        uniqLen = unique(sortedA.begin(), sortedA.end()) - sortedA.begin();
+        C.resize(uniqLen + 1, 0);
+        long long result = 0;
+
+        //discrete A[]
+        for (int i = 0; i < A.size(); ++i) {
+            A[i] = lower_bound(sortedA.begin(), sortedA.begin() + uniqLen, A[i]) - sortedA.begin() + 1;
+        }
+        
+        for (int i = 0; i < A.size(); ++i) {
+            result += sum(uniqLen) - sum(A[i]);
+            add(A[i], 1);
+        }
+        
+        return result;
+    }
+
+
+private:
+    int uniqLen;
+    vector<int> C;
+    
+    int lowbit(int x) {
+        return x & (-x);
+    }
+    
+    int sum(int x) {
+        int result = 0;
+        for (int i = x; i > 0; i -= lowbit(i)) {
+            result += C[i];
+        }
+        return result;
+    }
+    
+    void add(int x, int v) {
+        for (int i = x; i <= uniqLen; i += lowbit(i)) {
+            C[i] += v;
+        }
+    }
+};
---
+++ LintCode/522_Reverse-Pairs/README.md
@@ -0,0 +1,17 @@
+532. Reverse Pairs
+中文English
+Reverse pair is a pair of numbers (A[i], A[j]) such that A[i] > A[j] and i < j. Given an array, return the number of reverse pairs in the array.
+
+Example
+Example1
+
+Input:  A = [2, 4, 1, 3, 5]
+Output: 3
+Explanation:
+(2, 1), (4, 1), (4, 3) are reverse pairs
+Example2
+
+Input:  A = [1, 2, 3, 4]
+Output: 0
+Explanation:
+No reverse pair
