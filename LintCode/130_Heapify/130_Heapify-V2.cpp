6a01de88d8f035168e61fc02f3e1a6acfd088d16
luqian2017 <luqian.ncsu@gmail.com>
2019-11-25 22:43:11 -0800

    LintCode 130

---
+++ LintCode/130_Heapify/130_Heapify-V2.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    /*
+     * @param A: Given an integer array
+     * @return: nothing
+     */
+    void heapify(vector<int> &A) {
+        int n = A.size();
+        for (int i = (n - 2) / 2; i >= 0; --i)
+            min_heapify(A, i, n);
+    }
+    
+private:
+
+    void min_heapify(vector<int> &A, int i, int len) {
+        while(1) {
+            int left = 2 * i + 1;
+            int right = 2 * i + 2;
+            int smallest = i;
+            if (left < len && A[left] < A[i]) smallest = left;
+            if (right < len && A[right] < A[smallest]) smallest = right;
+        
+            if (smallest != i) {
+                swap(A[i], A[smallest]);
+                i = smallest;
+            } else {
+                break;
+            }
+        }
+    }
+
+
+};
