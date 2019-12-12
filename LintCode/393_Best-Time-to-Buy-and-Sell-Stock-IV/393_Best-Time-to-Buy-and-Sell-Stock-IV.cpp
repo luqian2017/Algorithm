a34339e07fe0be2f3ca9d255083a9edd695b1742
luqian2017 <luqian.ncsu@gmail.com>
2019-12-12 00:43:17 -0800

    LintCode 393

---
+++ LintCode/393_Best-Time-to-Buy-and-Sell-Stock-IV/393_Best-Time-to-Buy-and-Sell-Stock-IV.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    /**
+     * @param K: An integer
+     * @param prices: An integer array
+     * @return: Maximum profit
+     */
+    int maxProfit(int k, vector<int> &prices) {
+        int n = prices.size();
+        if (n == 0) return 0;
+    
+        if (k >= n / 2) {
+            int result = 0;
+            for (int i = 1; i < n; ++i) {
+                if (prices[i] - prices[i - 1] > 0)   
+                    result += prices[i] - prices[i - 1];
+            }
+        
+            return result;
+        }
+        
+        vector<int> global(k + 1);
+        vector<int> local(k + 1);
+        
+        for (int i = 0; i < prices.size() - 1; ++i) {
+            int diff = prices[i + 1] - prices[i];
+            for (int j = k; j >= 1; --j) {
+               local[j] = max(global[j - 1], local[j]) + diff;
+               global[j] = max(global[j], local[j]);
+            }
+        }
+        return global[k];
+    }
+};
