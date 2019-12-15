c5ea0678b4a623c3283c358529936278e4aba4c1
luqian2017 <luqian.ncsu@gmail.com>
2019-12-15 12:49:08 -0800

    LintCode 435

---
+++ LintCode/435_Post-Office-Problem/435_Post-Office-Problem.cpp
@@ -0,0 +1,48 @@
+#include <math.h>
+
+class Solution {
+public:
+    /**
+     * @param A: an integer array
+     * @param k: An integer
+     * @return: an integer
+     */
+    int postOffice(vector<int> &A, int k) {
+        int n = A.size();
+        if (n == 0 || k == 0 || k >= n) return 0;
+        
+        sort(A.begin(), A.end());
+        
+        vector<vector<int>> sumDis(n, vector<int>(n));
+
+        //sumDis[i][j] is the minSum of distance when [i, j] has 1 office
+        for (int i = 0; i < n; ++i) {
+            for (int j = i; j < n; ++j) {
+                int pos = A[(i + j) / 2];
+                for (int l = i; l <= j; ++l) {
+                    sumDis[i][j] += abs(A[l] - pos);
+                }
+            }
+        }
+
+        //dp[i][j] is the minimum cost that first i places have j offices
+        vector<vector<int>> dp(n + 1, vector<int>(k + 1, 0x3f3f3f3f)); //should not use INT_MAX here.
+        
+        //for beninning houses up to the first office, dp = 0 
+        for (int i = 0; i <= k; ++i) {
+            for (int j = 0; j <= i; ++j) {
+                dp[j][i] = 0;
+            }
+        }
+
+        for (int i = 1; i <= k; ++i) {
+            for (int j = i + 1; j <= n; ++j) {
+                for (int z = 1; z <= j; ++z) {
+                    dp[j][i] = min(dp[j][i], dp[j - z][i - 1] + sumDis[j - z][j - 1]);
+                }
+            }
+        }
+
+        return dp[n][k];
+    }
+};
---
+++ LintCode/435_Post-Office-Problem/README.md
@@ -0,0 +1,23 @@
+435. Post Office Problem
+中文English
+There are n houses on a line. Given an array A and A[i] represents the position of i-th house. Now you need to pick k position to build k post offices.
+
+What is the minimum sum distance from these n houses to the nearest post office?
+
+Example
+Example 1:
+
+Input: A = [1, 2, 3, 4, 5], k = 2
+Output: 3
+Explanation: Build post offices on position 2 and 4.
+Example 2:
+
+Input: A = [1, 2], k = 1
+Output: 1
+Explanation: Build post office on position 1 or 2.
+Challenge
+O(n^2) time
+
+Notice
+All positions are integers.
+
