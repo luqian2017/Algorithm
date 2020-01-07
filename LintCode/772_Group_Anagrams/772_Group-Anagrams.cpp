7f03430a9ae6cb04a2439dd30eacbdc4f377233f
luqian2017 <luqian.ncsu@gmail.com>
2020-01-06 21:56:59 -0800

    LintCode 772

---
+++ LintCode/772_Group_Anagrams/772_Group-Anagrams.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    /**
+     * @param strs: the given array of strings
+     * @return: The anagrams which have been divided into groups
+     */
+    vector<vector<string>> groupAnagrams(vector<string> &strs) {
+        int n = strs.size();
+        vector<vector<string>> result;
+        if (n == 0) return {{}};
+        
+
+        map<vector<int>, vector<string>> mp;   
+        //map<map<string, int>, vector<string>> mp;
+        
+        for (int i = 0; i < n; ++i) {
+            vector<int> dict(26, 0);
+          //  map<char, int> dict;
+            int m = strs[i].size();
+            for (int j = 0; j < m; ++j) {
+                dict[strs[i][j] - 'a']++;
+            }
+            mp[dict].push_back(strs[i]);
+        }
+        
+        for (auto m : mp) {
+            result.push_back(m.second);
+        }
+        
+        return result;
+    }
+};
---
+++ LintCode/772_Group_Anagrams/README.md
@@ -0,0 +1,23 @@
+772. Group Anagrams
+
+Given an array of strings, group anagrams together.
+
+Example
+Example 1:
+
+Input:
+["eat","tea","tan","ate","nat","bat"]
+Output:
+[["ate","eat","tea"],
+ ["bat"],
+ ["nat","tan"]]
+Example 2:
+
+Input:
+["eat","nowhere"]
+Output:
+[["eat"],
+ ["nowhere"]]
+Notice
+All inputs will be in lower-case.
+
