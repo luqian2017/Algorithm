e9b890fc151203b6f38e7511bac5adeac9836a0f
luqian2017 <luqian.ncsu@gmail.com>
2020-01-13 22:17:34 -0800

    LintCode 1311

---
+++ LintCode/1311_Lowest-Common-Ancestor-of-a-Binary-Search-Tree/1311_Lowest-Common-Ancestor-of-a-Binary-Search-Tree-V2.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition of TreeNode:
+ * class TreeNode {
+ * public:
+ *     int val;
+ *     TreeNode *left, *right;
+ *     TreeNode(int val) {
+ *         this->val = val;
+ *         this->left = this->right = NULL;
+ *     }
+ * }
+ */
+
+class Solution {
+public:
+    /**
+     * @param root: root of the tree
+     * @param p: the node p
+     * @param q: the node q
+     * @return: find the LCA of p and q
+     */
+    TreeNode * lowestCommonAncestor(TreeNode * root, TreeNode * p, TreeNode * q) {
+        if (!root) return NULL;
+        while(root) {
+            if (root->val > p->val && root->val > q->val) root = root->left;
+            else if (root->val < p->val && root->val < q->val) root = root->right;
+            else return root;
+        }
+    }
+};
