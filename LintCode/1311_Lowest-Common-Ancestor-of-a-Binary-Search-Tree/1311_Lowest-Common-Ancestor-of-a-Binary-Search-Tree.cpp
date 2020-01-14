d855e1d60d3b55d1d5a0f200283f5fab5b35d3b3
luqian2017 <luqian.ncsu@gmail.com>
2020-01-13 22:15:25 -0800

    LintCode 1311

---
+++ LintCode/1311_Lowest-Common-Ancestor-of-a-Binary-Search-Tree/1311_Lowest-Common-Ancestor-of-a-Binary-Search-Tree.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition of TreeNode:
+ * class TreeNode {
+ * public:
+ *     int val;
+ *     TreeNode *left, *right;
+ *     TreeNode(int val) {
+ *         this->val = val;
+ *         this->left = this->right = NULL;
+ *     }
+ * }
+ */
+
+class Solution {
+public:
+    /**
+     * @param root: root of the tree
+     * @param p: the node p
+     * @param q: the node q
+     * @return: find the LCA of p and q
+     */
+    TreeNode * lowestCommonAncestor(TreeNode * root, TreeNode * p, TreeNode * q) {
+        if (!root) return NULL;
+        if (root->val > p->val && root->val > q->val) return lowestCommonAncestor(root->left, p, q);
+        if (root->val < p->val && root->val < q->val) return lowestCommonAncestor(root->right, p, q);
+        return root;
+    }
+};
--- LintCode/779_Generalized-Abbreviation/README.md
+++ LintCode/779_Generalized-Abbreviation/README.md
@@ -15,3 +15,10 @@ Input:
 word = "today"
 Output:
 ["1o1a1","1o1ay","1o2y","1o3","1od1y","1od2","1oda1","1oday","2d1y","2d2","2da1","2day","3a1","3ay","4y","5","t1d1y","t1d2","t1da1","t1day","t2a1","t2ay","t3y","t4","to1a1","to1ay","to2y","to3","tod1y","tod2","toda1","today"]
+
+解法1：
+DFS。
+注意不用担心结果重复问题。
+Example 1:
+input = "aaaa"
+output = ["1a1a","1a2","1aa1","1aaa","2a1","2aa","3a","4","a1a1","a1aa","a2a","a3","aa1a","aa2","aaa1"]
