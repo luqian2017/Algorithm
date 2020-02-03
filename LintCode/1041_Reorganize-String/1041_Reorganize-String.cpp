c9a07e728f154acbf31a44a70db781f1eee12d99
luqian2017 <luqian.ncsu@gmail.com>
2020-02-03 00:10:22 -0800

    LintCode 1041

--- LintCode/1041_Reorganize-String/1041_Reorganize-String.cpp
+++ LintCode/1041_Reorganize-String/1041_Reorganize-String.cpp
@@ -1,4 +1,3 @@
-
 class Solution {
 public:
     /**
@@ -10,28 +9,30 @@ public:
         string result;
         if (n == 0) return result;
         
-        int thresh = (n & 0x1) ? n / 2 + 1 : n / 2;
+       // int thresh = (n & 0x1) ? n / 2 + 1 : n / 2;
         
         unordered_map<char, int> um;
-        int maxFreq = 0, maxId = 0;
+        priority_queue<pair<int, char>> pq;
+        
         for (int i = 0; i < n; ++i) {
             um[S[i]]++;
-            if (maxFreq < um[S[i]]) {
-                maxFreq = um[S[i]];
-                if (maxFreq > thresh) return result;
-                maxId = i;
-            }
+            if (um[S[i]] > (n + 1) / 2) return "";
         }
-      
-        result.resize(n, S[maxId]);
-        int p1 = 0, p2 = 1;
-        while(p1 < n && p2 < n) {
-            while (p1 < n && S[p1] == S[maxId]) p1++;
-            if (p1 == n) break;
-            result[p2] = S[p1++];
-            p2 += 2;
+        for (auto n : um) pq.push({n.second, n.first});
+        
+        while(pq.size() >= 2) {
+            pair<int, char> topA = pq.top(); pq.pop();
+            pair<int, char> topB = pq.top(); pq.pop();
+            result.push_back(topA.second); 
+            if (topA.first > 1) pq.push({topA.first - 1, topA.second});
+            result.push_back(topB.second);
+            if (topB.first > 1) pq.push({topB.first - 1, topB.second});
         }
         
+        if (pq.size() > 0) {
+            pair<int, char> top = pq.top(); pq.pop();
+            result.push_back(top.second);
+        }
         return result;
     }
 };
