556ecbccd2181a3d9c64c5c5113c0d4026c41bbe
luqian2017 <luqian.ncsu@gmail.com>
2020-02-06 00:30:52 -0800

    LintCode 8690

---
+++ LintCode/860_Number-of-Distinct-Islands/860_Number-of-Distinct-Islands.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    /**
+     * @param grid: a list of lists of integers
+     * @return: return an integer, denote the number of distinct islands
+     */
+    int numberofDistinctIslands(vector<vector<int>> &grid) {
+        int rowSize = grid.size();
+        int colSize = grid[0].size();
+        set<vector<pair<int, int>>> result;
+        
+        vector<int> dx = {0, 0, 1, -1};
+        vector<int> dy = {1, -1, 0, 0};
+        
+        for (int i = 0; i < rowSize; ++i) {
+            for (int j = 0; j < colSize; ++j) {
+                if (grid[i][j] == 0) continue;
+                vector<pair<int, int>> v;
+                queue<pair<int, int>> q;
+                q.push({i, j});
+                grid[i][j] = 0;
+                while(!q.empty()) {
+                    pair<int, int> top = q.front();
+                    q.pop();
+                    for (int i = 0; i < 3; ++i) {
+                        int newX = top.first + dx[i];
+                        int newY = top.second + dy[i];
+                        if (newX >= 0 && newX < rowSize && newY >= 0 && newY < colSize && grid[newX][newY] == 1) {
+                            q.push({newX, newY});
+                            v.push_back({newX - i, newY - j});
+                        }
+                    }
+                }
+                result.insert(v);
+            }
+        }
+        
+        return result.size();
+    }
+};
---
+++ LintCode/860_Number-of-Distinct-Islands/README.md
@@ -0,0 +1,44 @@
+860. Number of Distinct Islands
+
+Given a non-empty 2D array grid of 0's and 1's, an island is a group of 1's (representing land) connected 4-directionally (horizontal or vertical). You may assume all four edges of the grid are surrounded by water.
+
+Count the number of distinct islands. An island is considered to be the same as another if and only if one island has the same shape as another island (and not rotated or reflected).
+
+Notice that:
+
+11
+1
+and
+
+ 1
+11
+are considered different island, because we do not consider reflection / rotation.
+
+Example
+Example 1:
+
+Input: 
+  [
+    [1,1,0,0,1],
+    [1,0,0,0,0],
+    [1,1,0,0,1],
+    [0,1,0,1,1]
+  ]
+Output: 3
+Explanation:
+  11   1    1
+  1        11   
+  11
+   1
+Example 2:
+
+Input:
+  [
+    [1,1,0,0,0],
+    [1,1,0,0,0],
+    [0,0,0,1,1],
+    [0,0,0,1,1]
+  ]
+Output: 1
+Notice
+The length of each dimension in the given grid does not exceed 50.
