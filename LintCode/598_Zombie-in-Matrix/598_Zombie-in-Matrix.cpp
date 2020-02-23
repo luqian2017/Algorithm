e903fa1bc63d8a8a63521b55735b20ac7ce45c2e
luqian2017 <luqian.ncsu@gmail.com>
2020-02-23 00:08:15 -0800

    LintCode 598

--- LintCode/598_Zombie-in-Matrix/598_Zombie-in-Matrix.cpp
+++ LintCode/598_Zombie-in-Matrix/598_Zombie-in-Matrix.cpp
@@ -12,41 +12,35 @@ public:
         queue<pair<int, int>> q;
         for (int i = 0; i < m; ++i) {
             for (int j = 0; j < n; ++j) {
-                if (grid[i][j] == 1) {
-                    q.push({i, j});
-                }
+                if (grid[i][j] == 1) q.push({i, j});
             }
         }
-
-        vector<int> dx = {0, 0, 1, -1};
-        vector<int> dy = {1, -1, 0, 0};
+        
+        vector<int> dx = {1, -1, 0, 0};
+        vector<int> dy = {0, 0, 1, -1};
         int round = 0;
         while(!q.empty()) {
-            round++;
-            bool peopleFound = false; 
             int qSize = q.size();
+            round++;
             for (int i = 0; i < qSize; ++i) {
                 pair<int, int> frontNode = q.front();
                 q.pop();
-                
                 for (int j = 0; j < 4; ++j) {
                     int newX = frontNode.first + dx[j];
                     int newY = frontNode.second + dy[j];
                     if (newX >= 0 && newX < m && newY >= 0 && newY < n && grid[newX][newY] == 0) {
                         q.push({newX, newY});
                         grid[newX][newY] = 1;
-                        peopleFound = true;
                     }
                 }
             }
-            
-           // if (!peopleFound) return round;
-          //  round++;
         }
         
-        for (int i = 0; i < m; ++i) 
-            for (int j = 0; j < n; ++j)
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
                 if (grid[i][j] == 0) return -1;
+            }
+        }
         
         return round - 1;
     }
