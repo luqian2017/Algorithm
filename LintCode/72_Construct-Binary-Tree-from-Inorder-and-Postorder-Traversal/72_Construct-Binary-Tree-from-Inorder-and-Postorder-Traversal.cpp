4b073b4f09f274709740f43c3d4cdb58ce708160
luqian2017 <luqian.ncsu@gmail.com>
2020-03-05 23:54:32 -0800

    LintCode 72

---
+++ LintCode/72_Construct-Binary-Tree-from-Inorder-and-Postorder-Traversal/72_Construct-Binary-Tree-from-Inorder-and-Postorder-Traversal.cpp
@@ -0,0 +1,45 @@
+/**
+ * Definition of TreeNode:
+ * class TreeNode {
+ * public:
+ *     int val;
+ *     TreeNode *left, *right;
+ *     TreeNode(int val) {
+ *         this->val = val;
+ *         this->left = this->right = NULL;
+ *     }
+ * }
+ */
+
+class Solution {
+public:
+    /**
+     * @param inorder: A list of integers that inorder traversal of a tree
+     * @param postorder: A list of integers that postorder traversal of a tree
+     * @return: Root of a tree
+     */
+    TreeNode * buildTree(vector<int> &inorder, vector<int> &postorder) {
+        return helper(inorder, 0, inorder.size() - 1, postorder, 0, postorder.size() - 1);
+    }
+private:
+    TreeNode * helper(vector<int> &inorder, int inStart, int inEnd, vector<int> &postorder, int postStart, int postEnd) {
+        if (inStart > inEnd || inEnd < 0 || postEnd < 0) {
+            return NULL;
+        }
+        
+        TreeNode * root = new TreeNode(postorder[postEnd]);
+        int rootIndex = 0;
+        
+        for (int i = inStart; i <= inEnd; ++i) {
+            if (inorder[i] == postorder[postEnd]) {
+                rootIndex = i;
+                break;
+            }
+        }
+        int leftTreeCount = rootIndex - inStart;
+        int rightTreeCount = inEnd - rootIndex;
+        root->left = helper(inorder, inStart, rootIndex - 1, postorder, postStart, postStart + leftTreeCount - 1);
+        root->right = helper(inorder, rootIndex + 1, inEnd, postorder, postEnd - rightTreeCount, postEnd - 1);
+        return root;
+    }
+};
---
+++ LintCode/72_Construct-Binary-Tree-from-Inorder-and-Postorder-Traversal/README.md
@@ -0,0 +1,25 @@
+72. Construct Binary Tree from Inorder and Postorder Traversal
+
+Given inorder and postorder traversal of a tree, construct the binary tree.
+
+Example
+Example 1:
+
+Input：[],[]
+Output：{}
+Explanation:
+The binary tree is null
+Example 2:
+
+Input：[1,2,3],[1,3,2]
+Output：{2,1,3}
+Explanation:
+The binary tree is as follows
+  2
+ / \
+1   3
+
+Notice
+You may assume that duplicates do not exist in the tree.
+
+
