34ef2a9703897a753c9e1c3a6c9f9ed29f0e77cb
luqian2017 <luqian.ncsu@gmail.com>
2020-06-28 23:07:18 -0700

    Lintcode 1834

--- LintCode/1834_Grouping-Options/1834_Grouping-Options.cpp
+++ LintCode/1834_Grouping-Options/1834_Grouping-Options.cpp
@@ -8,15 +8,15 @@ public:
     long long groupingOptions(int n, int m) {
         if (n < m) return 0;
         //dp[i][j] is the number of options that i people are divided into j groups    
-        vector<vector<long long>> dp(n + 1, vector<long long>(n + 1));
-        for (int i = 1; i <= n; ++i) {
+        vector<vector<long long>> dp(n + 1, vector<long long>(m + 1));
+        for (int i = 1; i <= m; ++i) {
             dp[i][i] = 1;
         }
 
         for (int i = 2; i <= n; ++i) {
-            for (int j = 1; j < i; ++j) {
+            for (int j = 1; j <= min(i, m); ++j) {  //j<=m is also right
                 for (int k = 1; k <= j; ++k) {
-                    dp[i][j] += dp[i - j][k];                    
+                    dp[i][j] += dp[i - j][k];                
                 }
             }
         }
