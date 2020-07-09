05a4764c711355b545e6e316e04f5f1409318cdc
luqian2017 <luqian.ncsu@gmail.com>
2020-07-08 21:19:32 -0700

    Lintcode 379

---
+++ LintCode/379-Reorder-array-to-construct-the-minimum-number/379-Reorder-array-to-construct-the-minimum-number.cpp
@@ -0,0 +1,39 @@
+class compare {
+public:
+    bool operator() (const string & a, const string & b) {
+        string ab = a + b;
+        string ba = b + a;
+        return ab < ba;
+    }
+} cmp;
+
+
+class Solution {
+public:
+    /**
+     * @param nums: n non-negative integer array
+     * @return: A string
+     */
+    string minNumber(vector<int> &nums) {
+        int len = nums.size();
+        vector<string> strs(len);
+
+        for (int i = 0; i < len; ++i) {
+            strs[i] = to_string(nums[i]);
+        }
+        
+        sort(strs.begin(), strs.end(), cmp);
+        
+        string result;
+        for (int i = 0; i < len; ++i) {
+            result += strs[i];
+        }
+        
+        int non_zero_pos = 0;
+        for (int i = 0; i < result.size(); ++i) {
+            if (result[i] != '0') return result.substr(i);
+        }
+        
+        return "0"        
+    }
+};
---
+++ LintCode/379-Reorder-array-to-construct-the-minimum-number/README.md
@@ -0,0 +1,30 @@
+379. Reorder array to construct the minimum number
+Construct minimum number by reordering a given non-negative integer array. Arrange them such that they form the minimum number.
+
+Example
+Example 1:
+
+Input:[2,1]
+Output:[1,2]
+ Explanation: there are 2 possible numbers can be constructed by reordering the array:
+          1+2=12
+          2+1=21
+So after reordering, the minimum number is 12, and return it.
+Example 2:
+
+Input:[3, 32, 321]
+Output:[321, 32, 3]
+ Explanation: there are 6 possible numbers can be constructed by reordering the array:
+	3+32+321=332321
+	3+321+32=332132
+	32+3+321=323321
+	32+321+3=323213
+	321+3+32=321332
+	321+32+3=321323
+So after reordering, the minimum number is 321323, and return it.
+Challenge
+Do it in O(nlogn) time complexity.
+
+Notice
+The result may be very large, so you need to return a string instead of an integer.
+
