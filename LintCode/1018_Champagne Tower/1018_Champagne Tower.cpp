15f796c081b4925eddebf5cb53b99d2ce0a80ecd
luqian2017 <luqian.ncsu@gmail.com>
2020-07-28 01:56:12 -0700

    LintCode 1018

---
+++ LintCode/1018_Champagne Tower/1018_Champagne Tower.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    /**
+     * @param poured: an integer
+     * @param query_row: an integer
+     * @param query_glass: an integer
+     * @return: return a double
+     */
+    double champagneTower(int poured, int query_row, int query_glass) {
+
+        double result = 0.0;
+        
+        //dp[i][j] : amount of water filled into jth glass at row i
+        vector<vector<double>> dp(query_row + 1, vector<double>(query_row + 1, 0.0));
+        
+        dp[0][0] = poured;
+
+        for (int i = 1; i <= query_row; ++i) {
+            if (dp[i - 1][0] > 1) dp[i][0] = (dp[i - 1][0] - 1) / 2;
+            if (dp[i - 1][query_row - 1] > 1) dp[i][query_row - 1] = (dp[i - 1][query_row - 1] - 1) / 2;
+            for (int j = 1; j <= query_row; ++j) {
+                if (dp[i - 1][j - 1] > 1) dp[i][j] += (dp[i - 1][j - 1] - 1) / 2;
+                if (dp[i - 1][j] > 1) dp[i][j] += (dp[i - 1][j] - 1) / 2;  
+            }
+        }
+        
+        return dp[query_row][query_glass] > 1.0 ? 1.0 : dp[query_row][query_glass];
+    }
+};
