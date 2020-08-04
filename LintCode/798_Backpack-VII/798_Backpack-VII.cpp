13df35909d79faeba294e45ec92534b2c03941b1
luqian2017 <luqian.ncsu@gmail.com>
2020-08-03 23:46:34 -0700

    LintCode 798

--- LintCode/798_Backpack-VII/798_Backpack-VII.cpp
+++ LintCode/798_Backpack-VII/798_Backpack-VII.cpp
@@ -28,3 +28,63 @@ public:
         return dp[itemCount][n];
     }
 };
+
+
+//below is also OK
+class Solution {
+public:
+    /**
+     * @param n: the money of you
+     * @param prices: the price of rice[i]
+     * @param weight: the weight of rice[i]
+     * @param amounts: the amount of rice[i] 
+     * @return: the maximum weight
+     */
+    int backPackVII(int n, vector<int> &prices, vector<int> &weight, vector<int> &amounts) {
+        int itemCount = prices.size();   //count of items
+        vector<vector<int>> dp(itemCount + 1, vector<int>(n + 1, 0)); //dp[i][j] means the maximum weight of first i tems with total price <= j
+        
+        for (int i = 1; i <= itemCount; ++i) { 
+            for (int k = 1; k <= n; ++k) { //k->  1 .. n
+                dp[i][k] = dp[i - 1][k];
+                for (int j = 0; j <= amounts[i - 1]; ++j) {
+                    if (k >= j * prices[i - 1]) {
+                        dp[i][k] = max(dp[i][k], dp[i - 1][k - j * prices[i - 1]] + j * weight[i - 1]);
+                    }
+                }
+            }
+        }
+
+        return dp[itemCount][n];
+    }
+};
+
+//below is also OK
+class Solution {
+public:
+    /**
+     * @param n: the money of you
+     * @param prices: the price of rice[i]
+     * @param weight: the weight of rice[i]
+     * @param amounts: the amount of rice[i] 
+     * @return: the maximum weight
+     */
+    int backPackVII(int n, vector<int> &prices, vector<int> &weight, vector<int> &amounts) {
+        int itemCount = prices.size();   //count of items
+        vector<vector<int>> dp(itemCount + 1, vector<int>(n + 1, 0)); //dp[i][j] means the maximum weight of first i tems with total price <= j
+        
+        for (int i = 1; i <= itemCount; ++i) { 
+            for (int k = 1; k <= n; ++k) { //k->  1 .. n
+                for (int j = 0; j <= amounts[i - 1]; ++j) {
+                    if (k >= j * prices[i - 1]) {
+                        dp[i][k] = max(dp[i][k], dp[i - 1][k - j * prices[i - 1]] + j * weight[i - 1]);
+                    } else {
+                        dp[i][k] = max(dp[i - 1][k], dp[i][k]);
+                    }
+                }
+            }
+        }
+
+        return dp[itemCount][n];
+    }
+};
