823547c3ce18c38dc56b27cd52c01b976641d9d7
luqian2017 <luqian.ncsu@gmail.com>
2020-08-15 10:23:30 -0700

    LintCode 816

--- LintCode/816_Traveling-Salesman-Problem/816_Traveling-Salesman-Problem.cpp
+++ LintCode/816_Traveling-Salesman-Problem/816_Traveling-Salesman-Problem.cpp
@@ -6,43 +6,52 @@ public:
      * @return: return the minimum cost to travel all cities
      */
     int minCost(int n, vector<vector<int>> &roads) {
-        vector<vector<int>> cityLink(n, vector<int>(n, INT_MAX / 100));
+        int len = roads.size();
+        int inf = 1000000000;
+        graph.resize(n + 1, vector<int>(n + 1, inf));
+        constructGraph(roads, n);
+        
+        int state_size = (1 << n); //0000 - 1111
         
         //dp[i][j] : the min cost when state is i, the last passed city is j
-        vector<vector<int>> dp(n, vector<int>(1 << n, INT_MAX / 100));
+        vector<vector<int>> dp(state_size, vector<int>(n + 1, inf));
 
-        for (auto & road: roads){
-            int cityA = road[0] - 1, cityB = road[1] - 1, dist = road[2];
-            cityLink[cityA][cityB] = cityLink[cityB][cityA] = min(cityLink[cityA][cityB], dist);
-        }
-
-        priority_queue<pair<int, pair<int,int>>> maxHeap; //<cost, <city index, state>>, sort by cost
-        maxHeap.push({0, {0, 1}}); //init: the first city is visited
-      
-        dp[0][1] = 0; //cost of city 0 to itself is 0
-        while (!maxHeap.empty()){
-            auto now = maxHeap.top();
-            maxHeap.pop();
-    		int cost = now.first, city = now.second.first, state = now.second.second;
-    		
-    		if(cost > dp[city][state]) continue;
-    		
-    		for(int i = 0; i < n; ++i){
-    		    if ((state & (1 << i)) == 0) { //if city i has not been visited
-        			int new_state = (state | (1 << i)); //old state + city i
-        			if(dp[i][new_state] > dp[city][state] + cityLink[city][i]) {
-        				dp[i][new_state] = dp[city][state] + cityLink[city][i];
-        				maxHeap.push({dp[i][new_state], {i, new_state}});
-    			    }
-    		    }
-    		}
+        dp[1][1] = 0; // start from city 1, state is 0000001, minCost is 0
+        
+        for (int state = 0; state < state_size; ++state) {
+            for (int i = 2; i <= n; ++i) {
+                if ((state & (1 << (i - 1))) == 0) continue;
+                
+                int prev_state = state ^ (1 << (i - 1));
+                
+                for (int j = 1; j <= n; ++j) {
+                    if (i == j) continue;
+                    dp[state][i] = min(dp[state][i], dp[prev_state][j] + graph[j][i]);
+                }
+            }
         }
         
-        int minCost = dp[0][0];
-    	for(int i = 0; i < n; i++) {
-    		minCost = min(minCost, dp[i][(1 << n) - 1]);
-    	}
-    	
-    	return minCost;
+        int min_cost = inf;
+        for (int i = 1; i <= n; ++i) {
+            min_cost = min(min_cost, dp[state_size - 1][i]);
+        }
+        
+        return min_cost;
+    }
+    
+private:
+    vector<vector<int>> graph;
+    void constructGraph(vector<vector<int>> & roads, int n) {
+        int len = roads.size();
+        for (int i = 0; i < len; ++i) {
+            int source = roads[i][0];
+            int destination = roads[i][1];
+            int cost = roads[i][2];
+        //    graph[source][destination] = cost;
+        //    graph[destination][source] = cost;
+            graph[source][destination] = min(graph[source][destination], cost);
+            graph[destination][source] = min(graph[destination][source], cost);
+
+        }
     }
 };
