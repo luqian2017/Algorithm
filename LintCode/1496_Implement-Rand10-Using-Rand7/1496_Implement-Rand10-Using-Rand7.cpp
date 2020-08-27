2f50ce61aac60f72c740d435bbb50e80ff3481fb
luqian2017 <luqian.ncsu@gmail.com>
2020-08-27 01:04:40 -0700

    LintCode 1496

---
+++ LintCode/1496_Implement-Rand10-Using-Rand7/1496_Implement-Rand10-Using-Rand7.cpp
@@ -0,0 +1,12 @@
+// The rand7() API is already defined for you.
+// int rand7();
+// @return a random integer in the range 1 to 7
+class Solution :public SolBase {
+public:
+    int rand10() {
+        int sum1 = 0, sum2 = 0;
+        sum1 = rand7();
+        while ((sum2 = rand7()) > 3) continue;
+        return sum1 + sum2 - 1;
+    }
+};
