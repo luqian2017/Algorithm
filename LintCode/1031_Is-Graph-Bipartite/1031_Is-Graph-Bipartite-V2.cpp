ca2f9042f4b2851ca043520a9c2bd5023aaa6c5f
luqian2017 <luqian.ncsu@gmail.com>
2020-09-14 22:52:13 -0700

    LintCode 1031

---
+++ LintCode/1031_Is-Graph-Bipartite/1031_Is-Graph-Bipartite-V2.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    /**
+     * @param graph: the given undirected graph
+     * @return:  return true if and only if it is bipartite
+     */
+    bool isBipartite(vector<vector<int>> &graph) {
+        int n = graph.size();
+        vector<int> colors(n, 0);
+        for (int i = 0; i < n; ++i) {
+            if (colors[i] == 0 && !colored(i, graph, colors, 1)) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+private:
+    bool colored(int node, vector<vector<int>> &graph, vector<int> & colors, int color) {
+        colors[node] = color;
+        for (auto j : graph[node]) {
+            if (colors[j] == 0 && !colored(j, graph, colors, -color)) return false;
+            if (colors[j] == color) return false;
+        }
+
+        return true;
+    }
+};
--- LintCode/1031_Is-Graph-Bipartite/1031_Is-Graph-Bipartite.cpp
+++ LintCode/1031_Is-Graph-Bipartite/1031_Is-Graph-Bipartite.cpp
@@ -13,20 +13,21 @@ public:
         
         for (int i = 0; i < n; ++i) {
             if (set2.find(i) != set2.end()) {
-                for (int j = 0; j < graph[i].size(); ++j) {
-                    if (set2.find(graph[i][j]) != set2.end()) return false;
-                    set1.insert(graph[i][j]);
+                for (auto j : graph[i]) {
+                    if (set2.find(j) != set2.end()) return false;
+                    set1.insert(j);
                 }
             } else if (set1.find(i) != set1.end()) {
-                for (int j = 0; j < graph[i].size(); ++j) {
-                    if (set1.find(graph[i][j]) != set1.end()) return false;
-                    set2.insert(graph[i][j]);
+                for (auto j : graph[i]) {
+                    if (set1.find(j) != set1.end()) return false;
+                    set2.insert(j);
                 }
             } else {
+                //it is also ok to insert i into set2
                 set1.insert(i);
-                for (int j = 0; j < graph[i].size(); ++j) {
-                    if (set1.find(graph[i][j]) != set1.end()) return false;
-                    set2.insert(graph[i][j]);
+                for (auto j : graph[i]) {
+                    if (set1.find(j) != set1.end()) return false;
+                    set2.insert(j);
                 }
             }
         }
