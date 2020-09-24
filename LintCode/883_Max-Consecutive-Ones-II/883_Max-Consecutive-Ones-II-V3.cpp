4da56492269976d76af3144c117b90325ad54ab2
luqian2017 <luqian.ncsu@gmail.com>
2020-09-24 08:42:58 -0700

    LintCode 883

---
+++ LintCode/883_Max-Consecutive-Ones-II/883_Max-Consecutive-Ones-II-V3.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    /**
+     * @param nums: a list of integer
+     * @return: return a integer, denote  the maximum number of consecutive 1s
+     */
+    int findMaxConsecutiveOnes(vector<int> &A) {
+        int n = A.size();
+        
+        //dp_0: the maximum number of consecutive, end with A[i], never flipped so far
+        //dp_1: the maximum number of consecutive, end with A[i], flipped at most once
+        int dp_0 = 0, dp_1 = 0;
+        if (A[0] == 0) {
+            dp_0 = 0;
+            dp_1 = 1;
+        } else {
+            dp_0 = 1;
+            dp_1 = 1;
+        }
+        int max_len = 1;
+        for (int i = 1; i < n; ++i) {
+            if (A[i] == 0) {
+                dp_1 = dp_0 + 1;   //remember to bring dp_1 in the front of dp_0, otherwise it will be cleared.
+                dp_0 = 0;
+            } else {
+                dp_0++;
+                dp_1++;
+            }
+            max_len = max(max_len, dp_1);
+        }
+        
+       return max_len;
+    }
+};
