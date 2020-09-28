bbd86c9a0da1da11810da21de00e3bed6b4346aa
luqian2017 <luqian.ncsu@gmail.com>
2020-09-28 00:31:55 -0700

    LintCode 1295

--- LintCode/1295_Prime-Factor-Statistics/1295_Prime-Factor-Statistics.cpp
+++ LintCode/1295_Prime-Factor-Statistics/1295_Prime-Factor-Statistics.cpp
@@ -21,3 +21,26 @@ public:
         return result;
     }
 };
+
+//2nd try
+class Solution {
+public:
+    /**
+     * @param N: a number
+     * @return: the number of prime numbers.
+     */
+    int Count_PrimeNum(int N) {
+        int result = 0;
+        vector<int> records(N + 1, 1);
+        records[0] = 0, records[1] = 0;
+        for (int i = 2; i <= N; ++i) {
+            result += records[i];
+                int count = N / i;
+                for (int j = 1; j <= count; ++j) {
+                    records[i * j] = records[i] + records[j];
+                }
+        }
+        
+        return result;
+    }
+};
