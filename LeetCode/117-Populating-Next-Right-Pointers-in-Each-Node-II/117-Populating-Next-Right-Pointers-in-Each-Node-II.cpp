e2084a8aefbd3648184073b4edd1c7caf767fa3e
Qian <luqian.ncsu@gmail.com>
2021-08-18 23:13:28 -0700

    LeetCode 117

---
+++ LeetCode/117-Populating-Next-Right-Pointers-in-Each-Node-II/117-Populating-Next-Right-Pointers-in-Each-Node-II.cpp
@@ -0,0 +1,43 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+*/
+
+class Solution {
+public:
+    Node* connect(Node* root) {
+        if (!root) return NULL;
+        queue<Node *> q;
+        q.push(root);
+        while (!q.empty()) {
+            int qSize = q.size();
+            Node *tmpNode = q.front();
+            q.pop();
+            if (tmpNode->left) q.push(tmpNode->left);
+            if (tmpNode->right) q.push(tmpNode->right);
+            for (int i = 1; i < qSize; ++i) {
+                Node *frontNode = q.front();
+                q.pop();
+                if (frontNode->left) q.push(frontNode->left);
+                if (frontNode->right) q.push(frontNode->right);
+                
+                tmpNode->next = frontNode;
+                tmpNode = frontNode;
+            }
+        }
+        return root;
+    }
+};
